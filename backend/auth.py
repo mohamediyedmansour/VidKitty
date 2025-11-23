006e67726a33186ad576258f7ff6a80b78e38565
Mohamed Iyed Mansour <mohamediyedmansour@gmail.com>
2025-11-23 15:47:31 +0100

    Add proper login funcntionality and database

--- backend/auth.py
+++ backend/auth.py
@@ -1,39 +1,66 @@
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from database.session import SessionLocal
+from database.models import User
 from utils import verify_password, hash_password, create_jwt, decode_jwt
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-fake_db = {}
 
-def init_fake_user():
-    if "test@example.com" not in fake_db:
-        fake_db["test@example.com"] = hash_password("password123")
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-init_fake_user()
+
 
 @router.post("/register")
-def register(email: str, password: str):
-    if email in fake_db:
+def register(email: str, password: str, db: Session = Depends(get_db)):
+
+    exists = db.query(User).filter(User.email == email).first()
+    if exists:
         raise HTTPException(status_code=400, detail="Email already exists")
-    fake_db[email] = hash_password(password)
+
+    new_user = User(email=email, password=hash_password(password))
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
     return {"message": "User registered"}
 
 
-@router.post("/login")
-def login(form: OAuth2PasswordRequestForm = Depends()):
-    email = form.username
-    password = form.password
 
-    if email not in fake_db or not verify_password(password, fake_db[email]):
+@router.post("/login")
+def login(form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.email == form.username).first()
+
+    if not user or not verify_password(form.password, user.password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    token = create_jwt({"sub": email})
+    token = create_jwt({"sub": user.email})
+
     return {"access_token": token, "token_type": "bearer"}
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+
     payload = decode_jwt(token)
     if not payload:
         raise HTTPException(status_code=401, detail="Invalid token")
-    return payload
+
+    email = payload["sub"]
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="User no longer exists")
+
+    return user
